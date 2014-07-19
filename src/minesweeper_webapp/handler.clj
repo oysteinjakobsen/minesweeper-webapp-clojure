c7412067b32a429c5b179248c6bc466bc309606f
oysteinjakobsen <ojapublic@manglerud.net>
2014-07-19 14:39:41 +0200

    Reorganized code into smaller pieces. Added REST service for Hall of Fame.

--- src/minesweeper_webapp/handler.clj
+++ src/minesweeper_webapp/handler.clj
@@ -2,6 +2,7 @@
   (:use compojure.core)
   (:use minesweeper.core)
   (:use minesweeper.util)
+  (:use minesweeper.hof)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [cheshire.core :as json]
@@ -19,38 +20,60 @@
   [board]
   (assoc board :squares (map #(map square->map %) (:squares board))))
 
+(defn create-json-response
+  [response]
+  {:body (json/generate-string response)
+   :headers {"Content-Type" "application/json"}})
+
+(defn store-in-session
+  [response request map-to-be-stored]
+  (assoc response :session (merge (:session request) map-to-be-stored)))
+
+(defn non-cached-response
+  "Adds caching headers to the response in order to prevent browser caching."
+  [{:keys [headers] :as response}]
+  (assoc response 
+         :headers (assoc headers
+                         "Cache-Control" "no-cache, no-store, must-revalidate"
+                         "Pragma" "no-cache"
+                         "Expires" "-1")))
+
 (defn- create-board-response
   "Creates a JSON REST response based on the given request and new board. Board is stored on the session."
   [request board]
-  {:body (json/generate-string (restructure-board-for-json (restructure-board board)))
-   :headers {"Content-Type" "application/json"
-             "Cache-Control" "no-cache, no-store, must-revalidate"
-             "Pragma" "no-cache"
-             "Expires" "-1"}
-   :session (assoc (:session request) :board board)})
+  (->
+    (restructure-board-for-json (restructure-board board))
+    (create-json-response)
+    (non-cached-response)
+    (store-in-session request {:board board})))
 
 (defn- create-new-board
   "REST handler that creates and returns a new board of given size and number of mines."
-  [request]
-  (let [width (:width (:route-params request))
-        height (:height (:route-params request))
-        number-of-mines (:number-of-mines (:route-params request))
-        board (apply new-board (map read-string [width height number-of-mines]))]
-    (create-board-response request board)))
+  [{{:keys [width height number-of-mines]} :route-params :as request}]
+  (create-board-response 
+    request
+    (apply new-board (map read-string [width height number-of-mines]))))
 
 (defn- move
   "REST handler that performs the given action on the given coordinate and returns the updated board."
-  [request]
-  (let [coordinate (keyword (:coordinate (:route-params request)))
-        action (keyword (:action (:route-params request)))
-        board (:board (:session request))
-        new-board (do-move board coordinate action)]
-    (create-board-response request new-board)))
+  [{{:keys [coordinate action]} :route-params {:keys [board]} :session :as request}]
+  (create-board-response
+    request
+    (apply do-move board (map keyword [coordinate action]))))
+
+(defn- get-hof
+  "REST handler that returns Hall of Fame for given board size and number of mines."
+  [{{:keys [width height number-of-mines]} :route-params}]
+  (->
+    (apply get-hall-of-fame (map read-string [width height number-of-mines])) 
+    (create-json-response)
+    (non-cached-response)))
 
 (defroutes app-routes
   (GET "/" [] (response/redirect "/index.html"))
   (GET "/new/:width/:height/:number-of-mines" request (create-new-board request))
   (GET "/move/:coordinate/:action" request (move request))
+  (GET "/hof/:width/:height/:number-of-mines" request (get-hof request))
   (route/resources "/")
   (route/not-found "Not Found"))
 
